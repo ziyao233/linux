ae532854f29a081fa2a4b81dbd55a07b1ebc8797
Yixun Lan <dlan@gentoo.org>
2024-06-14 08:10:59 +0800

    cache: implement dcache's flush_all() via sbi call
    Signed-off-by: Yixun Lan <dlan@gentoo.org>

--- arch/riscv/kernel/cpu-hotplug.c
+++ arch/riscv/kernel/cpu-hotplug.c
@@ -15,6 +15,7 @@
 #include <asm/cpu_ops.h>
 #include <asm/numa.h>
 #include <asm/smp.h>
+#include <asm/sbi.h>
 
 bool cpu_has_hotplug(unsigned int cpu)
 {
@@ -67,9 +68,15 @@ void arch_cpuhp_cleanup_dead_cpu(unsigned int cpu)
 void __noreturn arch_cpu_idle_dead(void)
 {
 	idle_task_exit();
+#if defined(CONFIG_SOC_SPACEMIT_K1PRO) || defined(CONFIG_SOC_SPACEMIT_K1X)
+	sbi_flush_local_dcache_all();
+#endif
 
 	cpuhp_ap_report_dead();
 
+#if defined(CONFIG_SOC_SPACEMIT_K1PRO) || defined(CONFIG_SOC_SPACEMIT_K1X)
+	sbi_flush_local_dcache_all();
+#endif
 	cpu_ops->cpu_stop();
 	/* It should never reach here */
 	BUG();
